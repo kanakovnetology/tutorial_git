ee4c4dc1129fd89b19bdc9f7009d802ecc4281e7
kanakov <kanakov.netology@gmail.com>
2024-01-26 16:23:58 +0300

    the code was deleted

--- trial.py
+++ trial.py
@@ -1,6 +1,6 @@
 string0 = "Great "
 string1 = "Git "
 string2 = "Tutorials "
-joined_string = string0 + string1 + string2 + string0 + string1 + string2
+joined_string = string0 + string1 + string2
 print(string0)
 print(joined_string)
